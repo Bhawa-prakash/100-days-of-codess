7c0dcd5d91142917f416c73c64b47fa949d5f722
Bhawa prakash <35968702+Bhawa-prakash@users.noreply.github.com>
2018-08-02 21:52:44 +0530

    Create evenorodd.c

---
+++ code/c programs/evenorodd.c
@@ -0,0 +1,14 @@
+/* program to check whether a number is even and odd0  */
+main()
+{
+	int a;
+	clrscr();
+	printf("enter a number");
+	scanf("%d",&a);
+	if(a%==0)
+		printf("number is even");
+	else
+		printf("number is odd");
+	getch();
+	
+}
