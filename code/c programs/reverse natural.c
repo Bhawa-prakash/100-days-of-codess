f37ff9658cd6a1400e3f3acb6b55d51ccc2ac3e3
Bhawa prakash <35968702+Bhawa-prakash@users.noreply.github.com>
2018-08-04 17:22:07 +0530

    Create reverse natural.c

---
+++ code/c programs/reverse natural.c
@@ -0,0 +1,15 @@
+
+/* program to print n natural no .in reverse order  */
+main()
+{
+int i,N;
+printf("enter value of N");
+scanf("%d",N);
+for(i=N;i>=1;i--)
+{
+printf("%d",i);
+getch();
+}
+
+
+}
